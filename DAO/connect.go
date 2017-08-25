6f0e5be05eaf97c41b1d5344e9c6f000a4c19755
ChernovAndrey <chernov.andrey.98@mail.ru>
2017-08-26 01:50:34 +0300

    config add

--- DAO/connect.go
+++ DAO/connect.go
@@ -4,20 +4,41 @@ import (
 	"database/sql"
 	_ "github.com/lib/pq"
 	"fmt"
+	"path/filepath"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
 )
 
-const (
+/*const (
 	DB_USER     = "postgres"
 	DB_PASSWORD = "7680928160"
 	DB_NAME     = "textgame"
-)
+)*/
+type ConfigParameter struct {
+	DB_USER     string
+	DB_PASSWORD string
+	DB_NAME     string
 
+}
+var config ConfigParameter
+
+func init() {
+	filename, _ := filepath.Abs("./config/database.yml")
+	yamlFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("config DB",err)
+	}
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		fmt.Println("config DB",err)
+		}
+}
 
 var db *sql.DB
 
 func Connect() {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		DB_USER, DB_PASSWORD, DB_NAME)
+		config.DB_USER, config.DB_PASSWORD, config.DB_NAME)
 	var err error
 	db, err = sql.Open("postgres", dbinfo)
 	if(err!=nil){
--- webAppli.go
+++ webAppli.go
@@ -4,12 +4,35 @@ import (
 	"log"
 	"./DAO"
 	"./server"
+	"path/filepath"
+	"io/ioutil"
+	"fmt"
+	"gopkg.in/yaml.v2"
 )
 
+type Config struct {
+	Port string
+}
+var config Config
+
+func init(){
+	filename, _ := filepath.Abs("./config/server.yml")
+	yamlFile, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("config Port",err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		fmt.Println("config Port",err)
+	}
+
+}
 
 func main() {
 	DAO.Connect()
-	log.Fatal(server.RunHTTPServer(":8080"))
+	log.Fatal(server.RunHTTPServer(config.Port))
 	//http.HandleFunc("/signup", handlers.SignUp)
 	//http.HandleFunc("/signin", handlers.SignIn)
 	//log.Fatal(http.ListenAndServe(":8082", nil))
